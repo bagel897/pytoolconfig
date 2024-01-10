78e04e4c00c5a1dba1e80d9e1629cd2ab675ffdc
bagel897 <ellenagarwal897@gmail.com>
2024-01-10 13:16:12 -0600

    add enum tests

---
+++ tests/test_enum.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from pytoolconfig.pytoolconfig import PyToolConfig
+from enum import Enum
+
+
+class Demo(Enum):
+    DISABLED = False
+    ENABLED = True
+    ALT = "alternate"
+
+
+@dataclass
+class EnumModel:
+    option1: Demo = Demo.DISABLED
+    option2: Demo = Demo.DISABLED
+    option3: Demo = Demo.DISABLED
+
+
+def test_simple(cwd):
+    config = PyToolConfig("pytoolconfig2", cwd, EnumModel)
+    result = config.parse()
+    assert result.option1 == Demo.DISABLED
+    assert result.option2 == Demo.ENABLED
+    assert result.option3 == Demo.ALT
