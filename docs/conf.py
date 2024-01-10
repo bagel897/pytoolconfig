019f83afa1a2ecfb289aef65629fb9e952c7793d
bagel897 <ellenagarwal897@gmail.com>
2024-01-10 15:24:42 -0600

    fix warnings

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,10 @@ pygments_style = "sphinx"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
     "sphinx_autodoc_typehints",
     "pytoolconfig.documentation",
     "sphinx_rtd_theme",
@@ -56,4 +58,6 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
+autodoc_mock_imports = ["_typeshed"]
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
